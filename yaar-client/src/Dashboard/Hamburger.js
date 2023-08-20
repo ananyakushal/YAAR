1513682b63a0f1c4ae67fc51d82ab918c7d4e9fa
yashasvisxena <yashasvi1802@gmail.com>
2023-08-21 02:21:36 +0530

    added hamburger to mobile size display

--- yaar-client/src/Dashboard/Hamburger.js
+++ yaar-client/src/Dashboard/Hamburger.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import * as icons from "react-icons/fa";
+
 
 function Hamburger() {
-
   return (
-    <div className="hamburger md:hidden inline-block p-0 cursor-pointer">
-      <div className="line h-px w-3 my-1 bg-[--text_primary]"></div>
-      <div className="line h-px w-3 my-1 bg-[--text_primary]"></div>
-      <div className="line h-px w-3 my-1 bg-[--text_primary]"></div>
+    <div className="hamburger md:hidden mr-2">
+        <icons.FaBars />
     </div>
-  );
+  )
 }
 
 export default Hamburger;
--- yaar-client/src/Dashboard/RightPane/Rightpane.js
+++ yaar-client/src/Dashboard/RightPane/Rightpane.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import Hamburger from '../Hamburger'
-import Rightstats from './Rightstats'
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unreachable */
+import React from "react";
+import Hamburger from "../Hamburger";
+import Rightstats from "./Rightstats";
+import { useState } from "react";
 function Rightpane() {
+  const [view, toView] = useState(false);
   return (
-    <div className="right md:w-3/12 flex md:bg-[--body_paper] md:shadow-2xl md:rounded-lg flex-grow justify-center items-start">
-        <Hamburger/>
-        <Rightstats/>
+    <div className="right md:w-3/12 flex md:bg-[--body_paper] md:shadow-2xl md:rounded-lg flex-grow justify-start items-center flex-col z-10">
+      <button onClick={() => toView(!view)}>
+        <Hamburger />
+      </button>
+      {view ? (
+        <Rightstats className="Rightstats flex flex-col h-[95%] w-full text-xs md:text-sm lg:text-base mt-2 mb-4 justify-start align-centre text-left overflow-y-scroll" />
+      ) : (
+        <Rightstats className="Rightstats hidden md:flex flex-col h-[95%] w-full text-xs md:text-sm lg:text-base mt-2 mb-4 justify-start overflow-y-scroll" />
+      )}
     </div>
-  )
+  );
 }
 
-export default Rightpane
+export default Rightpane;
