7ba16497c25492da3231add43a1afbbba0ed9df7
ananyakushal <ananyakushal15@gmail.com>
2023-08-23 02:16:41 +0530

    Added preLoader files

--- yaar-client/src/App/App.js
+++ yaar-client/src/App/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App overflow-hidden">
       <Navbar />
       <Dashboard />
+      <preLoader />
       {/* <Footer/> */}
     </div>
   );
---
+++ yaar-client/src/pages/Landing/PreLoader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './preLoader.css'
+
+const preLoader = () => {
+  return (
+    <div className='preLoader'>
+        <div className="text-container">
+        <span> Think, </span>
+        <span> Save,</span>
+        <span> Spend, </span>
+    </div>
+    </div>
+  )
+}
+
+export default preLoader
