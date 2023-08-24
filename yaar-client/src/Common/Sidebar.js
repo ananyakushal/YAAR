415870878613fab0549f5711c96c9d34218c455d
yashasvisxena <yashasvi1802@gmail.com>
2023-08-24 23:19:28 +0530

    Added Routes

--- yaar-client/src/Common/Sidebar.js
+++ yaar-client/src/Common/Sidebar.js
@@ -1,68 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Sidebar.css"; // Import your CSS file for styling
-
+import { Link } from "react-router-dom";
 const Sidebar = (props) => {
-  const [selectedItem, setSelectedItem] = useState('Record'); // Initial selected item
+  const [selectedItem, setSelectedItem] = useState("Record"); // Initial selected item
 
   return (
     <div className={props.className}>
       <div className="profile-picture m-2 flex flex-row justify-center item-start text-sm md:text-lg">
         {/* Placeholder for profile picture */}
-        <img className="h-14 m-2 sm:h-16 rounded-full ring-4 ring-offset-[--body_background] ring-offset-4 ring-green-400 " src={props.img} alt="Profile" />
-        <h1 className='self-center ml-2'>Welcome {props.name} !!</h1>
+        <img
+          className="h-14 m-2 sm:h-16 rounded-full ring-4 ring-offset-[--body_background] ring-offset-4 ring-green-400 "
+          src={props.img}
+          alt="Profile"
+        />
+        <h1 className="self-center ml-2">Welcome {props.name} !!</h1>
       </div>
       <ul className="menu cursor-pointer list-none w-full m-3 text-sm md:text-base">
-        {/* <h1 className='p-[10px] text-lg w-full md:mb-2 text-center md:text-3xl'>Finance</h1> */}
-        <li
-          className={selectedItem === 'FinanceRecord' ? 'selected' : ''}
-          onClick={() => setSelectedItem('FinanceRecord')}
-        >
-          Finance Record
-        </li>
-        <li
-          className={selectedItem === 'Budget' ? 'selected' : ''}
-          onClick={() => setSelectedItem('Budget')}
-        >
-          Budget
-        </li>
-        <li
-          className={selectedItem === 'Categories' ? 'selected' : ''}
-          onClick={() => setSelectedItem('Categories')}
-        >
-          Categories
-        </li>
-        <li
-          onClick={() => setSelectedItem('Analysis')}
-          className={selectedItem === 'Analysis' ? 'selected' : ''}
-        >
-          Analysis
-        </li>
+        <Link to="/FinanceRecord">
+          <li
+            className={selectedItem === "FinanceRecord" ? "selected" : ""}
+            onClick={() => setSelectedItem("FinanceRecord")}
+          >
+            Finance Record
+          </li>
+        </Link>
+        <Link to="/Budget">
+          <li
+            className={selectedItem === "Budget" ? "selected" : ""}
+            onClick={() => setSelectedItem("Budget")}
+          >
+            Budget
+          </li>
+        </Link>
+        <Link to="/Categories">
+          <li
+            className={selectedItem === "Categories" ? "selected" : ""}
+            onClick={() => setSelectedItem("Categories")}
+          >
+            Categories
+          </li>
+        </Link>
+        <Link to="/Analysis">
+          <li
+            onClick={() => setSelectedItem("Analysis")}
+            className={selectedItem === "Analysis" ? "selected" : ""}
+          >
+            Analysis
+          </li>
+        </Link>
         <hr className="m-3" />
-        <li
-          className={selectedItem === 'TaskRecord' ? 'selected' : ''}
-          onClick={() => setSelectedItem('TaskRecord')}
-        >
-          Task Record
-        </li>
-        <li
-          className={selectedItem === 'Labels' ? 'selected' : ''}
-          onClick={() => setSelectedItem('Labels')}
-        >
-          Labels
-        </li>
-        <li
-          className={selectedItem === 'Progress' ? 'selected' : ''}
-          onClick={() => setSelectedItem('Progress')}
-        >
-          Progress
-        </li>
-        <hr className="m-3" />
-        <li
-          className={selectedItem === 'About' ? 'selected' : ''}
-          onClick={() => setSelectedItem('About')}
-        >
-          About Us
-        </li>
+        <Link to="/TaskRecord">
+          <li
+            className={selectedItem === "TaskRecord" ? "selected" : ""}
+            onClick={() => setSelectedItem("TaskRecord")}
+          >
+            Task Record
+          </li>
+        </Link>
+        <Link to="/Labels">
+          <li
+            className={selectedItem === "Labels" ? "selected" : ""}
+            onClick={() => setSelectedItem("Labels")}
+          >
+            Labels
+          </li>
+        </Link>
+        <Link to="/Progress">
+          <li
+            className={selectedItem === "Progress" ? "selected" : ""}
+            onClick={() => setSelectedItem("Progress")}
+          >
+            Progress
+          </li>
+        </Link>
+        {/* <hr className="m-3" />
+        <Link to="/About">
+          <li
+            className={selectedItem === "About" ? "selected" : ""}
+            onClick={() => setSelectedItem("About")}
+          >
+            About Us
+          </li>
+        </Link> */}
       </ul>
     </div>
   );
---
+++ yaar-client/src/pages/Dashboard/MainArea/Budget/Budget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Budget() {
+  return (
+    <div className='text-5xl'>Feature Coming Soon</div>
+  )
+}
+
+export default Budget;
