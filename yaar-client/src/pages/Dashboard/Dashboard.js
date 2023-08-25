fa2df921b7c5f5f1e8fa78f4f4a0de9b73187207
yashasvisxena <yashasvi1802@gmail.com>
2023-08-25 19:05:12 +0530

    Moved Navbar to Dashboard

--- yaar-client/src/pages/Dashboard/Dashboard.js
+++ yaar-client/src/pages/Dashboard/Dashboard.js
@@ -2,8 +2,11 @@ import React from 'react'
 import MainArea from './MainArea/MainArea'
 import Leftpane from './LeftPane/Leftpane'
 import Rightpane from './RightPane/Rightpane'
+import Navbar from '../../Common/Navbar'
 function Dashboard() {
   return (
+    <>
+    <Navbar/>
     <div className="dashboard ml-1 mr-1 h-[86vh] pt-4 w-screen flex flex-col mb-5 justify-center items-center overflow-hidden z-1">
       <h1 className="text-xl flex mt-1 mb-4 text-center sm:text-3xl ">My Dashboard</h1>
       <div className="panel flex flex-row h-full w-full space-x-1 pl-3 ">
@@ -12,6 +15,7 @@ function Dashboard() {
         <Rightpane/>
       </div>
     </div>
+    </>
   )
 }
 
