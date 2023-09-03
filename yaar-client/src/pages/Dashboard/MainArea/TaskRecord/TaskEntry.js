464154f44704dcac3a880f810a998521307fd333
yashasvisxena <yashasvi1802@gmail.com>
2023-09-04 01:46:40 +0530

    making task entry

--- yaar-client/src/pages/Dashboard/MainArea/TaskRecord/TaskEntry.js
+++ yaar-client/src/pages/Dashboard/MainArea/TaskRecord/TaskEntry.js
@@ -1,9 +1,9 @@
 import {useState,React} from 'react'
-import Daily from '../TaskRecord/labelicon/Daily.png'
-import Work from '../TaskRecord/labelicon/Work.png'
-import Urgent from '../TaskRecord/labelicon/Urgent.png'
-import Reminder from '../TaskRecord/labelicon/Reminder.png'
-import Events from '../TaskRecord/labelicon/Events.png'
+import Daily from '../TaskRecord/labelicons/Daily.png'
+import Work from '../TaskRecord/labelicons/Work.png'
+import Urgent from '../TaskRecord/labelicons/Urgent.png'
+import Reminder from '../TaskRecord/labelicons/Reminder.png'
+import Events from '../TaskRecord/labelicons/Event.png'
 const TaskEntry = (props) => {
   const categoryToImage = {
     Daily:Daily,
@@ -19,9 +19,15 @@ const TaskEntry = (props) => {
     <div className="Entry cursor-pointer bg-[--body_background] flex m-3 mb-1 mt-1 flex-row w-full rounded-lg">
         <img
         className="category h-8 m-2 sm:h-12 self-start"
-        src={categoryImage}
+        src={Daily}//put "categoryImage"
         alt={props.catText}
       />
+       <div className="Amt-time flex flex-row justify-center items-center mr-1">
+          <div className="Date-time text-xs md:text-sm">
+            <p className="text-center Date text-[0.5rem] md:text-base">{props.date}</p>
+            <p className="Time text-center text-[0.5rem] md:text-base">{props.time}</p>
+          </div>
+        </div>
     </div>
   )
 }
--- yaar-client/src/pages/Dashboard/MainArea/TaskRecord/TaskRecord.js
+++ yaar-client/src/pages/Dashboard/MainArea/TaskRecord/TaskRecord.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import TaskEntry from './TaskEntry';
 
 function TaskRecord() {
   return (
-    <div className='Records flex pb-2 mb-2 flex-col items-center justify-start w-full h-full overflow-y-scroll overflow-x-hidden z-0'></div>
+    <div className='Records flex pb-2 mb-2 flex-col items-center justify-start w-full h-full overflow-y-scroll overflow-x-hidden z-0'>
+      <TaskEntry  catText="Food" color="rgb(34 197 94)"  mode="Canara Bank" amount={500} date="25/12/23" time="05:00pm" />
+    </div>
   )
 }
 
