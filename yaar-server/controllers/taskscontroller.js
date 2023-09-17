3a0f5c5c784ef22b684d62d42728a6eca94aa608
TheXro <ravyyadav97@gmail.com>
2023-09-18 01:14:36 +0530

    finished tasks controllers

---
+++ yaar-server/controllers/taskscontroller.js
@@ -0,0 +1,38 @@
+const Task = require('../models/tasksmodel');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+} 
+
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No expense with id: ${taskID}` });
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    deleteTask
+}
