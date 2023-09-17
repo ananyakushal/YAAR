c4a43df8e74a4df1a4a66cf0e186900d164e26a7
TheXro <ravyyadav97@gmail.com>
2023-09-18 01:13:06 +0530

    task model/schema done

---
+++ yaar-server/models/tasksmodel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name cannot be more than 20 characters']
+    },
+    time: {
+        type: String,
+        required: [true, 'must provide date'],
+    }, 
+    date:{
+        type: String,
+        required: [true, 'must provide time'],
+    },
+    label:{
+        type: String,
+        required: [true, 'must provide label'],
+        enum: ["Work","Daily","Urgent","Events", "Reminders""]
+    },
+    id:{
+        type: String,
+        required: [true, 'must provide id'],
+    },
+    description:{
+        type: String,
+        required: [true, 'must provide description'],
+    },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
